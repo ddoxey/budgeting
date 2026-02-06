0d54a5f29b090f71605511816a98473d5b18cf44
Dylan Doxey <dylan.doxey@gmail.com>
2026-02-05 23:22:21 -0800

    Add format str for dotchart -y axis

--- ui/tables.py
+++ ui/tables.py
@@ -23,7 +23,7 @@ def make_dotchart(budget):
         return None
     rows, cols = Printer.screen_dims()
     proc = subprocess.run(
-        ['/usr/local/bin/dotchart', '-y',
+        ['/usr/local/bin/dotchart', '-y', '%0.0f',
                                     '--color',
                                     '--width', str(cols - 1)],
         input=data,
